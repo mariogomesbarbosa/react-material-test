a3024448c083bdefaf874e7a047f5b0e3b85c020
Mário Barbosa <mariobarbosa@MacBook-Pro-de-Mario.local>
2020-05-26 22:01:03 -0300

    installing material-ui and inserting textfield

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+
 function App() {
+
+  
   return (
     <div className="App">
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField id="outlined-basic" label="Segurado" placeholder="Nome e sobrenome do segurado" variant="outlined" />
       </header>
     </div>
   );
